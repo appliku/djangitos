6f4b7709e5b6c560f9d4b44db4c68b180e31ccd1
Servando Bermudez <servando@banzai.io>
2020-10-04 10:23:16 -0300

    Refactor Settings.py for better reading

--- djangito/settings.py
+++ djangito/settings.py
@@ -5,26 +5,33 @@ from pathlib import Path
 import environ
 from kombu import Queue
 
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
 env = environ.Env()
+"""
+Project Settings
+"""
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-SENTRY_DSN = env('SENTRY_DSN', default=None)
-if SENTRY_DSN:
-    import sentry_sdk
-    from sentry_sdk.integrations.django import DjangoIntegration
+# Allowed Hosts Definition
+if DEBUG:
+    # If Debug is True, allow all.
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
 
-    sentry_sdk.init(
-        dsn=SENTRY_DSN,
-        integrations=[
-            DjangoIntegration()
-        ],
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True
-    )
+"""
+Project Apps Definitions
+Django Apps - Django Internal Apps
+Third Party Apps - Apps installed via requirements.txt
+Project Apps - Project owned / created apps
 
+Installed Apps = Django Apps + Thrid Part apps + Projects Apps
+"""
 DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -60,6 +67,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Databases
+DATABASES = {
+    "default": env.db("DATABASE_URL")
+}
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
+DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
+
 ROOT_URLCONF = 'djangito.urls'
 
 TEMPLATES = [
@@ -93,11 +107,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# User Model Definition
 AUTH_USER_MODEL = 'usermodel.User'
+
+# Static files and Media Files Definition
 STATICFILES_STORAGE = 'djangito.storages.WhiteNoiseStaticFilesStorage'
-WHITENOISE_USE_FINDERS = True
-if DEBUG:
-    WHITENOISE_AUTOREFRESH = True
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_HOST = env('DJANGO_STATIC_HOST', default='')
+STATIC_URL = STATIC_HOST + '/static/'
 
 TIME_ZONE = 'UTC'
 LANGUAGE_CODE = 'en-us'
@@ -106,23 +125,44 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-SECRET_KEY = env('DJANGO_SECRET_KEY')
-if DEBUG:
-    ALLOWED_HOSTS = ['*']
-else:
-    ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
-
-DATABASES = {
-    "default": env.db("DATABASE_URL")
-}
-DATABASES["default"]["ATOMIC_REQUESTS"] = True
-DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
+# Admin URL Definition
 ADMIN_URL = env('DJANGO_ADMIN_URL', default='admin/')
 
+"""
+Thrid Party Settings
+"""
+# Sentry Settings
+SENTRY_DSN = env('SENTRY_DSN', default=None)
+
+if SENTRY_DSN:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+    from sentry_sdk.integrations.celery import CeleryIntegration
+    from sentry_sdk.integrations.redis import RedisIntegration
+
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[
+            DjangoIntegration(),
+            CeleryIntegration(),
+            RedisIntegration()
+        ],
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
+
+# Whitenose Settings
+# WHITENOISE_AUTOREFRESH = DEBUG -> Default behavior. 
+# http://whitenoise.evans.io/en/stable/django.html#WHITENOISE_AUTOREFRESH
+WHITENOISE_USE_FINDERS = True
+
+# Redis Settings
 REDIS_URL = env('REDIS_URL')
-CACHES = {
-    "default": env.cache('REDIS_URL')
-}
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_HOST = env('DJANGO_STATIC_HOST', default='')
-STATIC_URL = STATIC_HOST + '/static/'
+
+if REDIS_URL:
+    CACHES = {
+        "default": env.cache('REDIS_URL')
+    }
