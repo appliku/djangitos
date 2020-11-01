7e57389771f766ad458620a8f3fbb03148874fba
Konstantin P <konstantin.pavlovsky@gmail.com>
2020-11-01 18:31:07 +0100

    added django PublicMediaStorage that was missing

--- djangito/storages.py
+++ djangito/storages.py
@@ -1,5 +1,12 @@
+from storages.backends.s3boto3 import S3Boto3Storage
 from whitenoise.storage import CompressedManifestStaticFilesStorage
 
 
 class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
     manifest_strict = False
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = 'media'
+    default_acl = 'public-read'
+    file_overwrite = False
