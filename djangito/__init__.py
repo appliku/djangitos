711cde3b6300bb8d5ab4e77bd20e1ea1b6ff67f4
Kostja P <kpavlovsky@users.noreply.github.com>
2021-09-27 14:10:26 +0200

    Fixed incorrect import of celeryapp

--- djangito/__init__.py
+++ djangito/__init__.py
@@ -1,5 +1,5 @@
 try:
-    from .celery import app as celery_app
+    from .celeryapp import app as celery_app
     __all__ = ('celery_app',)
 except ModuleNotFoundError:
     print("Celery not installed, skipping")
